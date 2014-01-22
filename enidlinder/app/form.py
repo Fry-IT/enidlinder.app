3d0d499491f618deaa811a4b3a5e1e90d834672c
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2014-01-22 13:06:33 +0000

    Added custom view for form.py also wrap all field with fieldset

--- enidlinder/app/form.py
+++ enidlinder/app/form.py
@@ -4,204 +4,268 @@ from plone.app.textfield import RichText
 from plone.supermodel import model
 from zope import schema
 
-# fieldset
+# importing class to customising add view 
+from plone.dexterity.browser.add import DefaultAddForm, DefaultAddView
+from z3c.form import interfaces
+
+# importing for fieldset
 from plone.directives import form
 
 #datagrid for table inside form
-from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
+
 from zope import interface
 
 from plone.dexterity.browser.add import DefaultAddForm, DefaultAddView
 
+from z3c.form.browser.textlines import TextLinesFieldWidget
 
-# class ITableRowSchema(interface.Interface):
-    # current_year = schema.TextLine(title=u"Current year")
-    # following_year = schema.TextLine(title=u"Following year")
-    
+from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
+
 class IForm(model.Schema):
-	"""An application form for foundation. 
-	"""
-	
-	title = schema.TextLine(
+    """An application form for foundation. 
+    """
+    
+    form.fieldset('Content',
+        label=u"",
+        fields=['title', 'charity_number', 'address', 'telephone',
+            'email', 'website', 'description', 'project', 'project_aim']
+    )
+    
+    
+    title = schema.TextLine(
 				title=_(u"Name of Charity"),
 				)
 	
-	charity_number = schema.Int(
+    charity_number = schema.Int(
 			title=_(u"Registered Charity Number"),
 			required=False,
 			)
 			
-	address = schema.Text(
+    address = schema.Text(
 			title=_(u"Name and Address for Correspondence"),
 			required=False,
 			)
 
-	telephone = schema.TextLine(
+    telephone = schema.TextLine(
 			title=_(u"Telephone number"),
 			required=False,
 			)
 
-	email = schema.TextLine(
+    email = schema.TextLine(
 			title=_(u"Email address"),
 			required=False,
 			)
 
-	website = schema.TextLine(
+    website = schema.TextLine(
 			title=_(u"Website"),
 			required=False,
 			)
 
-	description = schema.Text(
+    description = schema.Text(
 			title=_(u"Brief statement of main objectives of your charity"),
 			required=False,
 			)
 
-	project = schema.Text(
+    project = schema.Text(
 			title=_(u"Name of Project"),
 			required=False,
 			)
 	
-	project_aim = schema.Text(
+    project_aim = schema.Text(
 			title=_(u"Aim of Project"),
 			required=False,
 			)
-	# form.widget(table=DataGridFieldFactory)  
-	# table = schema.List(title=u"Level of funding sought",
-				# value_type=DictRow(title=u"", 
-								   # schema=ITableRowSchema,
-								   # required=False))
+            
+    form.fieldset('default',
+        label=u"Level of funding sought",
+        fields=['c_funding_sought', 'f_funding_sought']
+    )        
 				
-	c_funding_sought = schema.TextLine(
-			title=_(u"Level of funding sought"),
-			description=_("current year"),
+    c_funding_sought = schema.TextLine(
+			title=_(u"Current year"),
+			required=False,
+			)   
+	
+    f_funding_sought = schema.TextLine(
+			title=_(u"Following year"),
+			required=False,
+			)
+	
+    form.fieldset('default',
+        label=u"Briefly of Project",
+        fields=['brief_project']
+    )  		
+    
+    brief_project = schema.Text(
+			title=_(u"Briefly describe your project and why it is needed and who will benefit"),
+			required=False,
+            )
+
+    form.fieldset('default',
+        label=u"Total Annual Income",
+        fields=['c_income', 'f_income']
+    )
+    
+
+    c_income = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	f_funding_sought = schema.TextLine(
-			title=_(u"Level of funding sought"),
-			description=_("next year"),
-			required=False,
-			)
-			
-			
-	brief_project = schema.Text(
-			title=_(u"Briefly of Project"),
-			description=_("Briefly describe your project and why it is needed and who will benefit"),
+    f_income = schema.TextLine(
+			title=_(u"Following year"),
 			required=False,
 			)
 
-	c_income = schema.TextLine(
-			title=_(u"Current year total annual income"),
-			required=False,
-			)
-			
-	f_income = schema.TextLine(
-			title=_(u"Following year total annual income"),
-			required=False,
-			)
+    form.fieldset('default',
+        label=u"Statutory Income",
+        fields=['c_statutory_income', 'f_statutory_income']
+    )
 		
-	c_statutory_income = schema.TextLine(
-			title=_(u"Current year statutory income"),
+    c_statutory_income = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	f_statutory_income = schema.TextLine(
-			title=_(u"Following year statutory income"),
+    f_statutory_income = schema.TextLine(
+			title=_(u"Following year"),
+			required=False,
+			)
+
+    form.fieldset('non_statutory_income',
+        label=u"Non-Statutory Income",
+        fields=['c_non_statutory_income', 'f_non_statutory_income']
+    )
+    
+    
+    c_non_statutory_income = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	c_non_statutory_income = schema.TextLine(
-			title=_(u"Current year non-statutory income"),
-			required=False,
-			)
-			
-	f_non_statutory_income = schema.TextLine(
-			title=_(u"Following year non-statutory income"),
+    f_non_statutory_income = schema.TextLine(
+			title=_(u"Following year "),
 			required=False,
 			)
 	
-	c_non_charitable_activities = schema.TextLine(
-			title=_(u"Total spent on current year on non-charitable activities"),
+    form.fieldset('non_charitable_activities',
+        label=u"Total spent on non-charitable activities",
+        fields=['c_non_charitable_activities', 'f_non_charitable_activities']
+    )
+    
+    c_non_charitable_activities = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	f_non_charitable_activities = schema.TextLine(
-			title=_(u"Total spent on following year on non-charitable activities"),
+    f_non_charitable_activities = schema.TextLine(
+			title=_(u"Following year"),
 			required=False,
 			)
-	c_spent_fundraising = schema.TextLine(
-			title=_(u"How much is spent current year on annually on fundraising"),
-			required=False,
-			)
-			
-	f_spent_fundraising = schema.TextLine(
-			title=_(u"How much is spent following year on annually on fundraising"),
-			required=False,
-			)
+
+    form.fieldset('spent_fundraising',
+        label=u"How much is spent annually on fundraising",
+        fields=['c_spent_fundraising', 'f_spent_fundraising']
+    )            
 	
-	c_income_expenditure = schema.TextLine(
-			title=_(u"Current year - Income & Expenditure balance - surplus or (deficit)"),
+    c_spent_fundraising = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	f_income_expenditure = schema.TextLine(
-			title=_(u"Following year - Income & Expenditure balance - surplus or (deficit)"),
+    f_spent_fundraising = schema.TextLine(
+			title=_(u"Following year"),
+			required=False,
+			)
+
+    form.fieldset('income_expenditure',
+        label=u"Income & Expenditure balance - surplus or (deficit)",
+        fields=['c_income_expenditure', 'f_income_expenditure']
+    ) 
+    
+    c_income_expenditure = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	c_cash_reserves = schema.TextLine(
-			title=_(u"Current year - Total cash reserves"),
+    f_income_expenditure = schema.TextLine(
+			title=_(u"Following year "),
+			required=False,
+			)
+
+    form.fieldset('cash_reserves',
+        label=u"Total cash reserves",
+        fields=['c_cash_reserves', 'f_cash_reserves']
+    )             
+			
+    c_cash_reserves = schema.TextLine(
+			title=_(u"Current year"),
 			required=False,
 			)
 			
-	f_cash_reserves = schema.TextLine(
-			title=_(u"Following year - Total cash reserves"),
+    f_cash_reserves = schema.TextLine(
+			title=_(u"Following year"),
+			required=False,
+			)
+
+    form.fieldset('realisable_assets',
+        label=u"Any other realisable assets? (e.g. land, property)",
+        fields=['c_realisable_assets', 'f_realisable_assets']
+    ) 
+			
+    c_realisable_assets = schema.TextLine(
+			title=_(u"Current year "),
 			required=False,
 			)
 			
-	c_realisable_assets = schema.TextLine(
-			title=_(u"Current year - Any other realisable assets? (e.g. land, property)"),
+    f_realisable_assets = schema.TextLine(
+			title=_(u"Following year"),
 			required=False,
 			)
-			
-	f_realisable_assets = schema.TextLine(
-			title=_(u"Following year - Any other realisable assets? (e.g. land, property)"),
-			required=False,
-			)
-	
-	trustees = schema.Text(
+	 
+    form.fieldset('',
+        label=u"",
+        fields=['trustees', 'supporters', 'previous_grant', 'hear']
+    ) 
+    
+    trustees = schema.Text(
 			title=_(u"Names of your trustees"),
 			required=False,
 			)
 	
-	supporters = schema.Text(
+    supporters = schema.Text(
 			title=_(u"Names of previous significant supporters"),
 			required=False,
 			)
 			
-	previous_grant  = schema.Text(
+    previous_grant  = schema.Text(
 			title=_(u"Details of any previous significant supporters"),
 			description=_(u"please list date(s) and amount(s)"),
 			required=False,
 			)
 	
-	hear = schema.Text(
+    hear = schema.Text(
 			title=_(u"How did you hear of the foundation?"),
 			required=False,
 			)
+
+    
 	
-	
-# class AddForm(DefaultAddForm):
-	# def updateWidgets(self):
-		# super(AddForm, self).updateWidgets()
-		# self.widgets['table'].allow_insert = False # Enable/Disable the insert button on the right
-		# self.widgets['table'].allow_delete = False # Enable/Disable the delete button on the right
-		# self.widgets['table'].auto_append = False  # Enable/Disable the auto-append feature
-		# self.widgets['table'].allow_reorder = False  # Enable/Disable the re-order rows feature
-# #		self.widgets['table'].main_table_css_class = 'my_custom_class'  # Change the class applied on the main table when the field is displayed
-		
-	
-	
-# class AddView(DefaultAddView):
-	# form = AddForm
+@form.default_value(field = IExcludeFromNavigation['exclude_from_nav'])
+def excludeFromNavDefaultValue(data):
+    return True    
+    
+class AddForm(DefaultAddForm):
+    
+    enable_form_tabbing = False
+    
+    def __init__(self, context, request, ti=None):
+        super(DefaultAddForm, self).__init__(context, request)
+        if ti is not None:
+            self.ti = ti
+            self.portal_type = ti.getId()
+        self.request['disable_border'] = False
+
+class AddView(DefaultAddView):
+	form = AddForm
 		
