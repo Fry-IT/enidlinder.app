b4d1fb6afdcc855be5fcb15b947b8f5dc0036c76
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2014-02-11 17:41:20 +0000

    Added new requirement in form

--- enidlinder/app/form.py
+++ enidlinder/app/form.py
@@ -23,234 +23,91 @@ from interfaces import IExcludeFromNavigationForm
 # import for edit form
 from plone.directives import dexterity, form
 
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 
 class IForm(model.Schema):
     """An application form for foundation. 
     """
     
-    form.fieldset('Content',
-        label=u"",
-        fields=['title', 'charity_number', 'address', 'telephone',
-            'email', 'website', 'description', 'project', 'project_aim']
-    )
     
     
     title = schema.TextLine(
-	title=_(u"Name of Charity"),
-	)
+        title=_(u"Name of Charity"),
+        )
 	
     charity_number = schema.Int(
-	title=_(u"Registered Charity Number"),
-	required=False,
-	)
-			
+        title=_(u"Registered Charity Number"),
+        required=False,
+        )
+    
+    aim_of_charity = schema.Text(
+        title=_(u"Aim of charity"),
+        description=_(u"(Maximum 50 words)"),
+        required=False,
+        )
+    
+    name_of_correspondent = schema.TextLine(
+        title=_(u"Name of Correspondent"),
+        required=False,
+        )
+	
+    position = schema.TextLine(
+        title=_(u"Position"),
+        required=False,
+        )
+    
     address = schema.Text(
-	title=_(u"Name and Address for Correspondence"),
-	required=False,
-	)
+        title=_(u"Name and Address for Correspondence"),
+        required=False,
+        )
 
     telephone = schema.TextLine(
-	title=_(u"Telephone number"),
-	required=False,
-	)
+        title=_(u"Telephone number"),
+        required=False,
+        )
 
     email = schema.TextLine(
-	title=_(u"Email address"),
-	required=False,
-	)
-
-    website = schema.TextLine(
-	title=_(u"Website"),
-	required=False,
-	)
-
-    description = schema.Text(
-	title=_(u"Brief statement of main objectives of your charity"),
-	required=False,
-	)
+        title=_(u"Email address"),
+        required=False,
+        )
 
     project = schema.Text(
-	title=_(u"Name of Project"),
-	required=False,
-	)
+        title=_(u"Title of Project"),
+        required=False,
+        )   
 	
-    project_aim = schema.Text(
-	title=_(u"Aim of Project"),
-	required=False,
-	)
+    aim_of_project = schema.Text(
+        title=_(u"Aim of Project"),
+        description=_(u"(Maximum 200 words)"),
+        required=False,
+        )
             
-    form.fieldset('default',
-        label=u"Level of funding sought",
-        fields=['c_funding_sought', 'f_funding_sought']
-    )        
-				
-    c_funding_sought = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)   
+    length_of_project = schema.TextLine(
+        title=_(u"Length of Project"),
+        required=False,
+        )
 	
-    f_funding_sought = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
+    project_benefit = schema.Text(
+        title=_(u"Who will benefit from the project?"),
+        required=False,
+        )
+    
+    money_applying = schema.TextLine(
+        title=_(u"How much are you applying for?"),
+        required=False,
+        )
 	
-    form.fieldset('default',
-        label=u"Briefly of Project",
-        fields=['brief_project']
-    )  		
-    
-    brief_project = schema.Text(
-	title=_(u"Briefly describe your project and why it is needed and who will benefit"),
-	required=False,
-            )
-
-    form.fieldset('default',
-        label=u"Total Annual Income",
-        fields=['c_income', 'f_income']
-    )
-    
-
-    c_income = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_income = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
+    money_raised = schema.TextLine(
+        title=_(u"How much have you raised towards the project?"),
+        required=False,
 	)
 
-    form.fieldset('default',
-        label=u"Statutory Income",
-        fields=['c_statutory_income', 'f_statutory_income']
-    )
-		
-    c_statutory_income = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_statutory_income = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
-
-    form.fieldset('non_statutory_income',
-        label=u"Non-Statutory Income",
-        fields=['c_non_statutory_income', 'f_non_statutory_income']
-    )
-    
-    
-    c_non_statutory_income = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_non_statutory_income = schema.TextLine(
-	title=_(u"Following year "),
-	required=False,
-	)
-	
-    form.fieldset('non_charitable_activities',
-        label=u"Total spent on non-charitable activities",
-        fields=['c_non_charitable_activities', 'f_non_charitable_activities']
-    )
-    
-    c_non_charitable_activities = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_non_charitable_activities = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
-
-    form.fieldset('spent_fundraising',
-        label=u"How much is spent annually on fundraising",
-        fields=['c_spent_fundraising', 'f_spent_fundraising']
-    )            
-	
-    c_spent_fundraising = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_spent_fundraising = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
-
-    form.fieldset('income_expenditure',
-        label=u"Income & Expenditure balance - surplus or (deficit)",
-        fields=['c_income_expenditure', 'f_income_expenditure']
-    ) 
-    
-    c_income_expenditure = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_income_expenditure = schema.TextLine(
-	title=_(u"Following year "),
-	required=False,
-	)
-
-    form.fieldset('cash_reserves',
-        label=u"Total cash reserves",
-        fields=['c_cash_reserves', 'f_cash_reserves']
-    )             
-			
-    c_cash_reserves = schema.TextLine(
-	title=_(u"Current year"),
-	required=False,
-	)
-			
-    f_cash_reserves = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
-
-    form.fieldset('realisable_assets',
-        label=u"Any other realisable assets? (e.g. land, property)",
-        fields=['c_realisable_assets', 'f_realisable_assets']
-    ) 
-			
-    c_realisable_assets = schema.TextLine(
-	title=_(u"Current year "),
-	required=False,
-	)
-			
-    f_realisable_assets = schema.TextLine(
-	title=_(u"Following year"),
-	required=False,
-	)
-	 
-    form.fieldset('',
-        label=u"",
-        fields=['trustees', 'supporters', 'previous_grant', 'hear']
-    ) 
-    
-    trustees = schema.Text(
-	title=_(u"Names of your trustees"),
-	required=False,
-	)
-	
-    supporters = schema.Text(
-	title=_(u"Names of previous significant supporters"),
-	required=False,
-	)
-			
-    previous_grant  = schema.Text(
-	title=_(u"Details of any previous significant supporters"),
-	description=_(u"please list date(s) and amount(s)"),
-	required=False,
-	)
-	
-    hear = schema.Text(
-	title=_(u"How did you hear of the foundation?"),
-	required=False,
-	)
+    supporting_docs = NamedBlobFile(
+		title = _(u"Supporting document"),
+        description = _(u"No more than 4 sides of A4 paper"),
+        required = False,
+		)
+   
 
             
 @form.default_value(field = IExcludeFromNavigationForm['exclude_from_nav'])
@@ -260,10 +117,19 @@ def excludeFromNavDefaultValue(data):
     
 class AddForm(DefaultAddForm):
     
-    enable_form_tabbing = False
+    enable_form_tabbing = True
     
     def updateWidgets(self):
         super(AddForm, self).updateWidgets()
+        
+        field_list = {	
+				'aim_of_charity':4,
+				'aim_of_project':10,
+		        'project_benefit':4,				
+                }
+        
+        for key,value in field_list.iteritems():
+		  	self.widgets[key].rows = value
         
              
 class AddView(DefaultAddView):
@@ -273,7 +139,17 @@ class AddView(DefaultAddView):
 class EditForm(dexterity.EditForm):
     
     grok.context(IForm)
-    enable_form_tabbing = False
+    enable_form_tabbing = True
 	
     def updateWidgets(self):
-        dexterity.EditForm.updateWidgets(self)    
+        dexterity.EditForm.updateWidgets(self)
+        
+        field_list = {	
+				'aim_of_charity':4,
+				'aim_of_project':10,
+		        'project_benefit':4,				
+                }
+        
+        for key,value in field_list.iteritems():
+		  	self.widgets[key].rows = value
+        
