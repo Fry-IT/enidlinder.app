f64eb3b1ac10069cbe312bc87ab410c6638e7f44
Radim Novotny <novotny.radim@gmail.com>
2014-10-09 09:13:08 +0200

    Copied and amended functionality from georgedrexler.grant

--- enidlinder/app/form.py
+++ enidlinder/app/form.py
@@ -1,11 +1,19 @@
+from Acquisition import aq_inner, aq_parent
+from AccessControl.SecurityManagement import getSecurityManager
 from enidlinder.app import MessageFactory as _
 from five import grok
 from plone.supermodel import model
 from zope import schema
-
+from Products.statusmessages.interfaces import IStatusMessage
+from plone.dexterity.events import AddCancelledEvent
+from zope.event import notify
 # importing class to customising add view
 from plone.dexterity.browser.add import DefaultAddForm, DefaultAddView
 from interfaces import IExcludeFromNavigationForm
+from z3c.form import button
+from plone.dexterity.utils import addContentToContainer
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import getUtility
 
 # import for edit form
 from plone.directives import dexterity, form
@@ -106,7 +114,7 @@ def excludeFromNavDefaultValue(data):
 
 
 class AddForm(DefaultAddForm):
-
+    immediate_view = '/apply'
     enable_form_tabbing = True
 
     def updateWidgets(self):
@@ -120,6 +128,26 @@ class AddForm(DefaultAddForm):
 
         for key, value in field_list.iteritems():
             self.widgets[key].rows = value
+
+    @button.buttonAndHandler(_('Save'), name='save')
+    def handleAdd(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        obj = self.createAndAdd(data)
+        if obj is not None:
+            # mark only as finished if we get the new object
+            self._finishedAdd = True
+            IStatusMessage(self.request).addStatusMessage(_(u"Please review your application and submit when ready"), "info")
+            self.request.response.redirect(self.nextURL())
+
+    @button.buttonAndHandler(_(u'Cancel'), name='cancel')
+    def handleCancel(self, action):
+        container = aq_parent(aq_inner(self.context))
+        IStatusMessage(self.request).addStatusMessage(_(u"Add New Item operation cancelled"), "info")
+        notify(AddCancelledEvent(self.context))
+        self.request.response.redirect(container.absolute_url())
 
 
 class AddView(DefaultAddView):
@@ -142,3 +170,47 @@ class EditForm(dexterity.EditForm):
 
         for key, value in field_list.iteritems():
             self.widgets[key].rows = value
+
+
+class AuthenticatedUser:
+    def __init__(self):
+        self.user = getSecurityManager().getUser()
+        if self.user:
+            self.user_type = self.user.getProperty('user_type')
+
+    def isIndividual(self):
+        if self.user_type == 'Individual':
+            return True
+        return False
+
+    def isMedicalSchool(self):
+        if self.user_type == 'Medical School':
+            return True
+        return False
+
+
+class View(grok.View):
+    grok.context(IForm)
+    grok.require('zope2.View')
+    grok.name('view')
+
+    def userIndividual(self):
+        auth_user = AuthenticatedUser()
+        if auth_user.isIndividual():
+            return True
+        return False
+
+    def userMedicalSchool(self):
+        auth_user = AuthenticatedUser()
+        if auth_user.isMedicalSchool():
+            return True
+        return False
+
+    def update(self):
+        self.review_state = self.context.portal_workflow.getInfoFor(self.context, 'review_state')
+
+    def canEdit(self):
+        return self.review_state == 'private'
+
+    def canSubmit(self):
+        return self.review_state == 'private'
